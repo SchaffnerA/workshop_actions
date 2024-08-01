dcdfa8d892decd09a0c4224f6f1f521adabfd25a
SchaffnerA <95848601+SchaffnerA@users.noreply.github.com>
2024-08-01 10:45:53 +0200

    Delete .github/workflows/tests directory

--- .github/workflows/tests/test_sample.py
+++
@@ -1,9 +0,0 @@
-""" this module aims to run some unit test"""
-
-import pandas
-def inc(x):
-    """adds 1 to the inputs parameter"""
-    return x+1
-def test_answer():
-    """Run a unit test of the inc function"""
-    assert inc(4)==5
