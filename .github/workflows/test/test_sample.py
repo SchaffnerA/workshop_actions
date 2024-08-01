b207a3457319e8f21f32a320abc2644b05d188a3
SchaffnerA <95848601+SchaffnerA@users.noreply.github.com>
2024-08-01 10:33:22 +0200

    Create test_sample.py

---
+++ .github/workflows/test/test_sample.py
@@ -0,0 +1,7 @@
+""" this module aims to run some unit test"
+def inx(x):
+    """adds 1 to the inputs parameter"""
+    return x+1
+def test_answer():
+    """Run a unit test of the inc function"""
+    assert inc(4)==5
